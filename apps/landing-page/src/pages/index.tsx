b7d646949b38432e9bf10440f92ff749c7c4f480
Dufyz <guilhermeiagothomaz@hotmail.com>
2024-01-06 19:11:53 -0300

    Configuring the start of the project

---
+++ apps/landing-page/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "@/layouts/RootLayout";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <RootLayout>
+      <>
+        <h1>Hello Wolrd, thats Your Finance</h1>
+      </>
+    </RootLayout>
+  );
+}
