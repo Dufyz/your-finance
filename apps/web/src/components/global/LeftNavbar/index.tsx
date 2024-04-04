d11289856e4c948312dc10685bfab8f7ec7f1510
Dufyz <guilhermeiagothomaz@hotmail.com>
2024-04-04 15:58:09 -0300

    Add @radix-ui/react-tabs package and update LeftNavbar component

--- apps/web/src/components/global/LeftNavbar/index.tsx
+++ apps/web/src/components/global/LeftNavbar/index.tsx
@@ -1,6 +1,6 @@
 import LeftNavbarItem from "./left-navbar-item";
 
-import {  IconCategory, IconCreditCard, IconFileInvoice, IconHeadset, IconManualGearbox, IconSettings, IconTargetArrow, IconWallet } from "@tabler/icons-react";
+import {  IconCategory, IconCreditCard, IconFileInvoice, IconHeadset, IconManualGearbox, IconSettings, IconTargetArrow, IconTransfer, IconWallet } from "@tabler/icons-react";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 
@@ -15,13 +15,11 @@ const LeftNavbar = () => {
               <h1 className="text-2xl text-green-700 font-semibold">YourFinance</h1>
             </div>
             
-
-
-
           </div>
               <Separator  />
           <div className="w-full flex flex-1 flex-col items-start justify-start gap-4">
           <LeftNavbarItem icon={<IconCategory size={24} color="#166534" />} label="Dashboard" route="/dashboard"/>
+          <LeftNavbarItem icon={<IconTransfer size={24} color="#166534" />} label="Transactions" route="/transactions"/>
           <LeftNavbarItem icon={<IconWallet size={24} color="#166534" />} label="Wallets" route="/wallets"/>
           <LeftNavbarItem icon={<IconCreditCard size={24} color="#166534" />} label="Cards" route="/cards"/>
           <LeftNavbarItem icon={<IconFileInvoice size={24} color="#166534" />} label="Invoices" route="/invoices"/>
