e1440a327fb1a0d2c40ca75d059be3ef16f600e0
Dufyz <guilhermeiagothomaz@hotmail.com>
2024-04-13 13:26:28 -0300

    Add is_resolved column to SupportForms table

--- supabase/migrations/20240413160532_create-table-support-forms.sql
+++ supabase/migrations/20240413160532_create-table-support-forms.sql
@@ -3,6 +3,7 @@ CREATE TABLE SupportForms (
     user_id INT REFERENCES Users (id) NOT NULL,
     category_id INT NOT NULL,
     message TEXT NOT NULL,
+    is_resolved BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
